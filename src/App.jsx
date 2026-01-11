427d9af430e8ebb800a8818010cee6bc82fe7319
IrynaPiatnochka <ivpiatnochka@gmail.com>
2026-01-11 16:21:05 -0600

    Add reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import {useState} from "react"
-import TodoList from "./TodoList.jsx"
-import TodoForm from "./TodoForm.jsx"
-import './App.css'
+import { useState } from 'react';
+import TodoList from './features/TodoList/TodoList.jsx';
+import TodoForm from './features/TodoForm.jsx';
+import './App.css';
 
 function App() {
-
   const [todoList, setTodoList] = useState([]);
 
   function addTodo(title) {
@@ -17,27 +16,32 @@ function App() {
     setTodoList([...todoList, newTodo]);
   }
 
-
   function completeTodo(todoId) {
-    const updatedTodos = todoList.map ((todo) => {
+    const updatedTodos = todoList.map((todo) => {
       if (todo.id === todoId) {
-        return { ...todo, isCompleted: true};
+        return { ...todo, isCompleted: true };
       }
       return todo;
     });
 
     setTodoList(updatedTodos);
-  
+  }
+
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map((todo) =>
+      todo.id === editedTodo.id ? { ...editedTodo } : todo
+    );
+
+    setTodoList(updatedTodos);
   }
 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo}/>
     </div>
   );
 }
-  
 
-export default App
+export default App;
